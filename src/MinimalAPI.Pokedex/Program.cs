6f6da76f23be5e36cefcdec7e11443e7b16f59af
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2021-11-11 07:56:14 +0100

    readme update

--- src/MinimalAPI.Pokedex/Program.cs
+++ src/MinimalAPI.Pokedex/Program.cs
@@ -28,7 +28,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pokedex Api v1");
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pokedex API v1");
         c.RoutePrefix = string.Empty;
     });
 }
